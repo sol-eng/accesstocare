35a9af3c862288e895415fe614bb31500f76ff9c
Edgar Ruiz <edgararuiz@gmail.com>
2021-09-08 16:00:35 -0500

    Switches to cards

--- inst/content/connectwidgets/app.R
+++ inst/content/connectwidgets/app.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(connectwidgets)
-#library(shinydashboard)
 library(shinymaterial)
 library(dplyr)
 library(stringr)
@@ -9,68 +8,95 @@ library(readr)
 library(accesstocare)
 
 
-if(Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect") {
+if (Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect") {
   connect_server <- Sys.getenv("CONNECT_SERVER")
   connect_key <- Sys.getenv("CONNECT_API_KEY")
-  client <- connect(server  = connect_server, api_key = connect_key)
-  all_content <- content(client)   
+  client <- connect(server = connect_server, api_key = connect_key)
+  all_content <- content(client)
 } else {
-  if(!file_exists("content.rds")) {
+  if (!file_exists("content.rds")) {
     connect_server <- Sys.getenv("CONNECT_SERVER")
     connect_key <- Sys.getenv("CONNECT_API_KEY")
-    client <- connect(server  = connect_server, api_key = connect_key)
-    all_content <- content(client) 
+    client <- connect(server = connect_server, api_key = connect_key)
+    all_content <- content(client)
     write_rds(all_content, "content.rds")
   } else {
     all_content <- read_rds("content.rds")
   }
 }
 
-choice_types <- c("All", "Script", "Report", "Dashboard", "Data", "Model", 
-                  "Notebook", "Plot"
-                  )
+choice_types <- c(
+  "All", "Script", "Report", "Dashboard", "Data", "Model",
+  "Notebook", "Plot"
+)
 
-atc_content <- all_content %>% 
-  by_tags("Access to Care") %>% 
+atc_content <- all_content %>%
+  by_tags("Access to Care") %>%
   mutate(
-    title = str_remove(title, "Access to Care - "), 
+    title = str_remove(title, "Access to Care - "),
     type = case_when(
       content_category == "pin" ~ "Data",
       TRUE ~ "Script"
+    ),
+    language = case_when(
+      str_detect(app_mode, "python") ~ "python",
+      TRUE ~ "R"
     )
-    ) 
+  )
 
 
 ui <- material_page(
   title = "Access to Care",
   primary_theme_color = palette_atc$ok,
-  secondary_theme_color = palette_atc$below,
-  material_side_nav(
-    fixed = TRUE, 
-    material_radio_button(
-      input_id = "type",
-      label = "Content Type",
-      choices = choice_types
-    ),
-
+  secondary_theme_color = palette_atc$above,
+  background_color = "white",
+  material_parallax("hospital1.png"),
+  fluidRow(
+    absolutePanel(
+      material_radio_button(
+        "type",
+        label = "Content Type",
+        choices = choice_types
+      ),
+      right = 20,
+      top = 70
+    )
   ),
-  material_parallax("hospital.jpeg"),
-  material_row(
-    rscgridOutput("grid")
+  fluidRow(
+    absolutePanel(
+      material_radio_button(
+        input_id = "language",
+        label = "Language",
+        choices = c("All", "R", "python")
+      ),
+      right = 200,
+      top = 70
+    )
+  ),
+  fluidRow(
+    absolutePanel(
+      rsccardOutput("grid", width = "90%"),
+      left = 200,
+      width = "80%"
+    )
   )
 )
-  
-server <- function(input, output, session) {
-  
-  output$grid <- renderRscgrid({
-    if(input$type != "All") {
-      fa <- atc_content %>% 
-        filter(type == input$type)
-    } else {
-      fa <- atc_content
-    }
-    rsc_grid(fa)
-  })
 
+server <- function(input, output, session) {
+  output$grid <- renderRsccard({
+    fa <- atc_content
+    
+    if (input$type != "All") fa <- filter(fa, type == input$type)
+    
+    if (input$language != "All") fa <- filter(fa, language == input$language)
+    
+    if (nrow(fa) > 0) {
+      rsc_card(fa)
+    } else {
+      NULL
+    }
+  })
+  
+  
 }
 shinyApp(ui = ui, server = server)
