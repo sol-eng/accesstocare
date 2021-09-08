2cb7fbc015d265e3c955e81cd26cdd14c0bfed20
Edgar Ruiz <edgararuiz@gmail.com>
2021-09-08 16:23:50 -0500

    Classifies rest of content

--- inst/content/connectwidgets/app.R
+++ inst/content/connectwidgets/app.R
@@ -27,7 +27,7 @@ if (Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect") {
 
 choice_types <- c(
   "All", "Script", "Report", "Dashboard", "Data", "Model",
-  "Notebook", "Plot"
+  "Notebook", "Plot", "Presentation"
 )
 
 atc_content <- all_content %>%
@@ -35,10 +35,18 @@ atc_content <- all_content %>%
   mutate(
     title = str_remove(title, "Access to Care - "),
     type = case_when(
+      str_detect(title, "Jupyter|RNotebook") ~ "Notebook",
+      content_category == "pin" & str_detect(title, "Model") ~ "Model",
       content_category == "pin" ~ "Data",
-      TRUE ~ "Script"
+      str_detect(title, " Prep") ~ "Script",
+      str_detect(title, "Presentation|PowerPoint") ~ "Presentation",
+      app_mode == "rmd-static" ~ "Report", 
+      app_mode %in% c("rmd-shiny", "python-dash") ~ "Dashboard",
+      app_mode == "static" ~ "Plot", 
+      TRUE ~ "Other"
     ),
     language = case_when(
+      str_detect(title, "Jupyter") ~ "python",
       str_detect(app_mode, "python") ~ "python",
       TRUE ~ "R"
     )
@@ -50,32 +58,24 @@ ui <- material_page(
   primary_theme_color = palette_atc$ok,
   secondary_theme_color = palette_atc$above,
   background_color = "white",
-  material_parallax("hospital1.png"),
+  material_parallax("hospital.jpeg"),
   fluidRow(
     absolutePanel(
-      material_radio_button(
-        "type",
-        label = "Content Type",
-        choices = choice_types
-      ),
-      right = 20,
+      material_radio_button("type", "Content Type", choice_types),
+      right = 80,
       top = 70
     )
   ),
   fluidRow(
     absolutePanel(
-      material_radio_button(
-        input_id = "language",
-        label = "Language",
-        choices = c("All", "R", "python")
-      ),
-      right = 200,
+      material_radio_button("language", "Language", c("All", "R", "python")),
+      right = 300,
       top = 70
     )
   ),
   fluidRow(
     absolutePanel(
-      rsccardOutput("grid", width = "90%"),
+      rsccardOutput("cards", width = "90%"),
       left = 200,
       width = "80%"
     )
@@ -83,20 +83,18 @@ ui <- material_page(
 )
 
 server <- function(input, output, session) {
-  output$grid <- renderRsccard({
+  output$cards <- renderRsccard({
     fa <- atc_content
-    
+
     if (input$type != "All") fa <- filter(fa, type == input$type)
-    
+
     if (input$language != "All") fa <- filter(fa, language == input$language)
-    
+
     if (nrow(fa) > 0) {
       rsc_card(fa)
     } else {
       NULL
     }
   })
-  
-  
 }
 shinyApp(ui = ui, server = server)
